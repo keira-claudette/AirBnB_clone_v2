499af024f6356fdeb6186674f85e65fab7da7598
keira-claudette <claudette.keira@gmail.com>
2021-09-12 10:24:07 +0000

    changed display message for route /

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Returns a string to the root route /"""
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
